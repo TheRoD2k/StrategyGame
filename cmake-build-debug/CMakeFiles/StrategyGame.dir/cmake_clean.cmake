4c6d505b554381eb080faf6971e56b14eb227f34
RoD <Ponomarev.Artem@phystech.edu>
2019-03-27 00:51:39 +0300

    Basic commit

---
+++ cmake-build-debug/CMakeFiles/StrategyGame.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StrategyGame.dir/main.cpp.o"
+  "StrategyGame.pdb"
+  "StrategyGame"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/StrategyGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
